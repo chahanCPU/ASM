9127cfd0e5a1980e6467133325f08c41cf7ce8ed
shosy <ilikesgh22@gmail.com>
2020-02-19 00:22:40 +0900

    不要なファイルを削除

--- assembly_old/fputest.s
+++
@@ -1,7 +0,0 @@
-min_caml_start:
-    lui.s   $f8, 0x4080     # 4.000000の上位16bits
-    lui.s   $f3, 0x43c8     # 400.000000の上位16bits
-    lui.s   $f4, 0x3fc0     # 1.500000の上位16bits
-    ftoi $f5, $f3
-    out $f5
-    
